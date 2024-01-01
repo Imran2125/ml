ff001be279d35ead39c8616504b7889ffb599e69
IMRAN ULLA BAIG K <85021611+Imran2125@users.noreply.github.com>
2024-01-01 21:27:55 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,98 @@
+def recaostar(n):
+    print("expanding node:",n)
+    and_nodes=[]
+    or_nodes=[]
+    if n in all_nodes:
+        if 'AND' in all_nodes[n]:
+            and_nodes=all_nodes[n]['AND']
+        if 'OR' in all_nodes[n]:
+            or_nodes=all_nodes[n]['OR']
+    if len(and_nodes)==0 and len(or_nodes)==0:
+        return 
+    solvable=False
+    marked={}
+    while not solvable:
+        if len(marked)==len(and_nodes)+len(or_nodes):
+            min_cost_least,min_cost_least_group=least_cost_group(and_nodes,or_nodes,marked)
+            solvable=True
+            change_heuristic(n,min_cost_least)
+            optimal_child_group[n]=min_cost_least_group
+            continue
+        min_cost,min_cost_group=least_cost_group(and_nodes,or_nodes,marked)
+        is_expanded=False
+        if len(min_cost_group)>1:
+            if min_cost_group[0] in all_nodes:
+                is_expanded=True
+                recaostar(min_cost_group[0])
+            if min_cost_group[1] in all_nodes:
+                is_expanded=True
+                recaostar(min_cost_group[1])
+        else:
+            is_expanded=True
+            recaostar(min_cost_group)
+        
+        if is_expanded:
+            min_cost_verify,min_cost_group_verify=least_cost_group(and_nodes,or_nodes,{})
+            if min_cost_group==min_cost_group_verify:
+                solvable=True
+                change_heuristic(n,min_cost_verify)
+                optimal_child_group[n]=min_cost_group
+        else:
+            solvable=True
+            change_heuristic(n,min_cost)
+            optimal_child_group[n]=min_cost_group
+        marked[min_cost_group]=1
+    return heuristic(n)
+
+def heuristic(n):
+    return h_dist[n]
+
+def change_heuristic(n,cost):
+    h_dist[n]=cost
+    return 
+
+def least_cost_group(and_nodes,or_nodes,marked):
+    node_wise_cost={}
+    for node_pair in and_nodes:
+        if not node_pair[0] in marked and not node_pair[1] in marked:
+            cost=0
+            cost+=heuristic(node_pair[0])+heuristic(node_pair[1])+2
+            node_wise_cost[node_pair[0]+node_pair[1]]=cost
+    for node in or_nodes:
+        if not node in marked:
+            cost=0
+            cost+=heuristic(node)+1
+            node_wise_cost[node]=cost
+    min_cost=99999
+    min_cost_group=None
+    for costkey in node_wise_cost:
+        if node_wise_cost[costkey]<min_cost:
+            min_cost=node_wise_cost[costkey]
+            min_cost_group=costkey
+    return [min_cost,min_cost_group]
+
+def print_path(node):
+    print(optimal_child_group[node],end=" ")
+    node=optimal_child_group[node]
+    if len(node)>1:
+        if node[0] in optimal_child_group:
+            print('->',end=" ")
+            print_path(node[0])
+        if node[1] in optimal_child_group:
+            print("->",end=" ")
+            print_path(node[1])
+    else:
+        if node in optimal_child_group:
+            print("->",end=" ")
+            print_path(node)
+    
+h_dist={'A':-1,'B':4,'C':2,'D':3,'E':6,'F':8,'G':2,'H':0,'I':0,'J':0}
+all_nodes={'A':{'AND':[('C','D')],'OR':['B']},
+          'B':{'OR':['E','F']},
+          'C':{'AND':[('H','I')],'OR':['G']},
+          'D':{'OR':['J']}
+          }
+optimal_child_group={}
+optimal_cost=recaostar('A')
+print_path('A')
+print('\n optimal cost is \n',optimal_cost)
