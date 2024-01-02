688af72a4dfed570175236194e80a9e84a59b272
IMRAN ULLA BAIG K <85021611+Imran2125@users.noreply.github.com>
2024-01-02 09:30:12 +0530

    Update naive.py

--- naive.py
+++ naive.py
@@ -1,14 +1,13 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 mush=pd.read_csv("mushroom.csv")
+print(len(mush.columns),"Columns after dropping NA",len(mush.dropna(axis=1).columns))
 mush.replace('?',np.nan,inplace=True)
-print(len(mush.columns),"columns after dropping NA",len(mush.dropna(axis=1).columns))
 mush.dropna(axis=1,inplace=True)
-target='class'
+target="class"
 features=mush.columns[mush.columns!=target]
-
 classes=mush[target].unique()
-test=mush.sample(frac=0.4)
+test=mush.sample(frac=0.3)
 mush=mush.drop(test.index)
 probs={}
 probcl={}
@@ -17,33 +16,30 @@ for x in classes:
     clsp={}
     tot=len(mushcl)
     for col in mushcl.columns:
-        colp={}
+        clp={}
         for val,cnt in mushcl[col].value_counts().items():
             pr=cnt/tot
-            colp[val]=pr
-            clsp[col]=colp
+            clp[val]=pr
+        clsp[col]=clp
     probs[x]=clsp
     probcl[x]=len(mushcl)/len(mush)
-
-
-def probas(x):
+def probabs(x):
     if not isinstance(x,pd.Series):
-        raise IOError('Arg must of type series')
+        raise IOError("Arg must of type series")
     probab={}
     for cl in classes:
         pr=probcl[cl]
-        for col,val in x.items():  
+        for col,val in x.items():
             try:
                 pr*=probs[cl][col][val]
-            except KeyError:
+            except:
                 pr=0
-            probab[cl]=pr
-        return probab
-
+        probab[cl]=pr
+    return probab
 def classify(x):
-    probab=probas(x)
+    probab=probabs(x)
     mx=0
-    mxcl=0
+    mxcl=""
     for cl,pr in probab.items():
         if pr>mx:
             mx=pr
@@ -53,9 +49,9 @@ b=[]
 for i in mush.index:
     b.append(classify(mush.loc[i,features])==mush.loc[i,target])
 print(sum(b),"correct of",len(mush))
-print("Acuuracy:",sum(b)/len(mush))
+print("accuracy:",sum(b)/len(mush))
 b=[]
 for i in test.index:
     b.append(classify(test.loc[i,features])==test.loc[i,target])
 print(sum(b),"correct of",len(test))
-print("Accuracy:",sum(b)/len(test))
+print("accuracy:",sum(b)/len(test))
